ff214cfb0a44d76fb409be921c308040f2dad53f
ydb <graceydb@qq.com>
2018-08-10 16:19:07 +0800

    update 111

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
               <i class="el-icon-location"></i>
               <span>影片管理</span>
             </template>
-            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-1">选项111</el-menu-item>
             <el-menu-item index="1-2">选项2</el-menu-item>
             <el-menu-item index="1-3">选项3</el-menu-item>
           </el-submenu>
